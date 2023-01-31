9b49e96f806e4f0696e8b794c044d41b1ea9a535
anderkuo <alexanderkuo0614@gmail.com>
2023-01-30 17:26:59 -0800

    Update Limelight.java

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.ArrayList;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -11,36 +9,71 @@ import frc.robot.Constants;
 
 public class Limelight extends SubsystemBase {
   private final NetworkTable limelightTable;
-  private double tv, tx, ta;
-  private ArrayList<Double> targetList;
-  private final int MAX_ENTRIES = 50;
-  private final NetworkTableEntry isTargetValid, ledEntry;
+  private double tv, tx, ty, ta;
+  private final NetworkTableEntry ledEntry;
+  private boolean targetExists = false;
+  private final double kP = 0.03; //check
+  private final double AIM_THRESHOLD = 1.0;
 
 
   /**
    * Creates a new Vision.
    */
   public Limelight() {
-    limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
-    targetList = new ArrayList<Double>(MAX_ENTRIES);
-    isTargetValid = ShuffleboardInfo.getInstance().getTargetEntry();
+    limelightTable = NetworkTableInstance.getDefault().getTable("limelight"); 
     ledEntry = limelightTable.getEntry("ledMode");
+    
+  }
+
+  public double findDistance() {
+    double targetOffsetAngle_Vertical = ty;
+    // how many degrees back is your limelight rotated from perfectly vertical?
+    double limelightMountAngleDegrees = 25.0; //TODO CHANGE
+    // distance from the center of the Limelight lens to the floor
+    double limelightLensHeightInches = 20.0; //TODO CHANGE
+    // distance from the target to the floor
+    double goalHeightInches = 60.0; //TODO CHANGE
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+    //calculate distance
+    double distanceFromLimelightToGoalInches = (goalHeightInches-limelightLensHeightInches)/Math.tan(angleToGoalRadians);
+    return distanceFromLimelightToGoalInches;
+  }
+
+  public double autoAim()
+  {
+      double heading_error = tx; //Might need to be negative
+      double steering_adjusment = 0.0;
+      if(tx > AIM_THRESHOLD)
+      {
+          steering_adjusment = kP * heading_error;
+      }
+      else if(tx < AIM_THRESHOLD)
+      {
+          steering_adjusment = kP * heading_error;
+      }
+      double rotation = steering_adjusment;
+      return rotation;
   }
 
   @Override
   public void periodic() {
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
+    tv = limelightTable.getEntry("tv").getDouble(0);
+    tx = limelightTable.getEntry("tx").getDouble(0);
+    ty = limelightTable.getEntry("ty").getDouble(0);
+    ta = limelightTable.getEntry("ta").getDouble(0);
+
+    if (tv < 1) {
+      targetExists = false;
+    }
+    else {
+      targetExists = true;
+    }
+
     //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);    
+    SmartDashboard.putNumber("LimelightX", tx);
+    SmartDashboard.putNumber("LimelightY", ty);
+    SmartDashboard.putNumber("LimelightArea", ta);    
   }
 
   
