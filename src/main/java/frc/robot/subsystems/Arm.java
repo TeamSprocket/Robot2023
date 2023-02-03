94fea2a5fd83ef94d1382df574b10bd603bd91f5
CoreyZhong <coreyzhong@gmail.com>
2023-02-02 17:17:09 -0800

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,7 +7,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
-
 public class Arm extends SubsystemBase {
     private final CANSparkMax armMotor_top = new CANSparkMax(0,MotorType.kBrushless);
     private final CANSparkMax armMotor_bottom = new CANSparkMax(0, MotorType.kBrushless);
@@ -17,5 +16,15 @@ public class Arm extends SubsystemBase {
         armMotor_bottom.setInverted(false);
     }
 
-    
+    public void setOutput(double output) {
+        armMotor_top.set(output);
+        armMotor_bottom.set(output);
+    }
+
+    public void stop() {
+        armMotor_top.stopMotor();
+        armMotor_bottom.stopMotor();
+    }
+
+
 }
