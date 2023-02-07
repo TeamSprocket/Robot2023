cd0ebb905876b64c25ce1ccac258a3568f512414
CoreyZhong <coreyzhong@gmail.com>
2023-02-06 17:35:55 -0800

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,29 +1,28 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotMap;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class Arm extends SubsystemBase {
-    private final CANSparkMax armMotor_top = new CANSparkMax(0,MotorType.kBrushless);
-    private final CANSparkMax armMotor_bottom = new CANSparkMax(0, MotorType.kBrushless);
+    private final CANSparkMax armMotor_Left = new CANSparkMax(RobotMap.Arm.ARM_LEFT,MotorType.kBrushless);
+    private final CANSparkMax armMotor_Right = new CANSparkMax(RobotMap.Arm.ARM_RIGHT, MotorType.kBrushless);
     
     public Arm(){
-        armMotor_top.setInverted(false);
-        armMotor_bottom.setInverted(false);
+        armMotor_Left.setInverted(false);
+        armMotor_Right.setInverted(false);
     }
 
     public void setOutput(double output) {
-        armMotor_top.set(output);
-        armMotor_bottom.set(output);
+        armMotor_Left.set(output);
+        armMotor_Right.set(output);
     }
 
     public void stop() {
-        armMotor_top.stopMotor();
-        armMotor_bottom.stopMotor();
+        armMotor_Left.stopMotor();
+        armMotor_Right.stopMotor();
     }
 
 
