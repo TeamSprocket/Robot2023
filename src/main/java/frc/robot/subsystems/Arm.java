a3b430ce19ae83f5db9d6092c991ac4143af789c
kimjason832 <kimjason.832@gmail.com>
2023-02-13 19:45:58 -0800

    Arm Updates

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -8,6 +8,7 @@ import com.revrobotics.SparkMaxRelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -16,52 +17,58 @@ public class Arm extends SubsystemBase {
     
     private final CANSparkMax armLeft = new CANSparkMax(RobotMap.Arm.ARM_LEFT,MotorType.kBrushless);
     private final CANSparkMax armRight = new CANSparkMax(RobotMap.Arm.ARM_RIGHT, MotorType.kBrushless);
+
+    private PIDController armPIDController = new PIDController(Constants.Arm.P, Constants.Arm.I, Constants.Arm.D);
+    
     private RelativeEncoder armLeftEncoder = armLeft.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096);
     private RelativeEncoder armRightEncoder = armRight.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096); 
-    private SparkMaxPIDController armPIDController = armLeft.getPIDController();
+
 
     public Arm(){
-        armLeft.setInverted(false);
-        armRight.setInverted(false);
-
-        armPIDController.setP(Constants.Arm.P);
-        armPIDController.setI(Constants.Arm.I);
-        armPIDController.setD(Constants.Arm.D);
-        armPIDController.setIZone(Constants.Arm.Iz);
-        armPIDController.setFF(Constants.Arm.FF);
-        armPIDController.setOutputRange(0,0);
-
-
-    }
-
-    public void config(){
         armLeft.restoreFactoryDefaults();
         armRight.restoreFactoryDefaults();
-    
-        armRight.follow(armLeft, true);
+        
+        armLeft.setInverted(false);
 
+        armRight.follow(armLeft, false);
 
-        armPIDController.setFeedbackDevice(armLeftEncoder);
-    }
-
-    public void setOutputArm(double output) {
-        armLeft.set(output);
-        armRight.set(output);
+        // armPIDController.setOutputRange(0,0);
     }
 
     public void setOutputPID(double setpoint){
-        armPIDController.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+        double output;
     }
 
 
     
     //use encoder values to get the height of the arm
     //choose a specific point on the arm(the end of the arm) to measure the height with the angle
-    public double ArmPosition() {
-        //double circum = 2 * Math.PI * (Units.inchesToMeters(5.731) / 2); 
-        //double circum = 2 * Math.PI * (0.05371 / 2); 
+    public double getArmPosition() {
+        double motorPos = armLeftEncoder.getPositionConversionFactor();
+        double sprocketPos = motorPos / Constants.Elevator.kElevatorGearRatio;
 
-        return 2;
+        double armPosRad = sprocketPos * (2 * Math.PI);
+        return armPosRad;
+
+        //double circum = 2 * Math.PI * (Units.inchesToMeters(5.731) / 2); 
+        // return 2;
+    } 
+
+    public void setArmPosition(double setpoint){
+        double output = armPIDController.calculate(
+            getArmPosition(),
+            setpoint);
+        //TODO ADD ELEVATOR HEIGHT CHANGES
+        
+
+
+        if (getArmPosition() >= (0.8 * Constants.Arm.HEIGHT_METERS_NO_ELEVATOR)
+        || getArmPosition() <= (0.2 * Constants.Arm.HEIGHT_METERS_NO_ELEVATOR)) {
+            output *= (Math.abs(getArmPosition() - setpoint) / Constants.Elevator.HEIGHT_METERS);
+        }
+        
+        output /= Constants.Elevator.MAX_SPEED;
+        armLeft.set(output);
     }
 
     public void stop() {
@@ -72,7 +79,7 @@ public class Arm extends SubsystemBase {
 
     @Override
     public void periodic(){
-        SmartDashboard.putNumber("Encoder Position", armLeftEncoder.getPosition());
+        SmartDashboard.putNumber("Encoder Position", armLeftEncoder.getPosition()); 
         SmartDashboard.putNumber("Encoder Position", armRightEncoder.getPosition());
     }
 }
