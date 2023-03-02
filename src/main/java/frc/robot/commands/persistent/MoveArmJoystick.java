c6fc74f9e8e7910c04f4d8239a62b0203a2bf716
kimjason832 <kimjason.832@gmail.com>
2023-03-02 15:45:05 -0800

    Code Cleanup

--- src/main/java/frc/robot/commands/persistent/MoveArmJoystick.java
+++ src/main/java/frc/robot/commands/persistent/MoveArmJoystick.java
@@ -21,33 +21,13 @@ public class MoveArmJoystick extends PersistentCommand {
     @Override
     public void execute() {
 
-      double rightY = gamepad.getRightY();
+      double input = gamepad.getRightY();
 
-      // double rightY = gamepad.getRightY();
-      double armInput = Util.deadband(0.1, rightY);
-      //   System.out.println(rightY);
-      // System.out.println(arm.getArmAngle());
-      
-      // if (armInput == 0) {
-        // double output = armInput;
-        // output += 0.00224556 * arm.getArmAngle() + 0.103286;
-        // if (arm.getArmAngle() > -60 && arm.getArmAngle() < 0) {
-        //   output -= 0.03;
-        //   if (arm.getArmAngle() > -30 && arm.getArmAngle() < 0) {
-        //     output -= 0.01;
-        //   }
-        // }
-      //   arm.moveArm(output);
-      // }
-      // else {
-      //   arm.moveArm(armInput);
-      // }
+      double armInput = Util.deadband(0.1, input);
+
       double newSetpoint = (5 * armInput) + arm.getArmAngle();
       System.out.println("ARM ANGLE: " + arm.getArmAngle());
       if (armInput == 0){
-        // if (arm.getArmAngle() < 0 && arm.getArmAngle() > -1) {
-        //   arm.setArmAngle(arm.getArmAngle(), newSetpoint+0.5);
-        // }
         if (arm.getArmAngle() < -1 && arm.getArmAngle() > -80) {
           double output = armInput;
           output += 0.00224556 * arm.getArmAngle() + 0.083286;
@@ -65,11 +45,6 @@ public class MoveArmJoystick extends PersistentCommand {
             output += 0.025;
           }
 
-          // if (arm.getArmAngle() > -60 && arm.getArmAngle() < 0) {
-          //   output -= 0.03;
-          // if (arm.getArmAngle() > -30 && arm.getArmAngle() < 0) {
-          //   output -= 0.01;
-          // }
           arm.moveArm(output);
         }
         
--- src/main/java/frc/robot/commands/persistent/MoveWristManual.java
+++ src/main/java/frc/robot/commands/persistent/MoveWristManual.java
@@ -24,24 +24,9 @@ public class MoveWristManual extends PersistentCommand {
       
       double input = gamepad.getLeftY();
 
-      // double input = gamepad.getLeftTriggerAxis() - gamepad.getRightTriggerAxis(); 
-
       double wristInput = Util.deadband(0.1, input);
       
-      // System.out.println("angle: " + wrist.getWristAngle());
-    //   System.out.println("output: " + input);
-      
-      // if (wristInput == 0){
-      //   double output = wristInput;
-      //   output += 0.000482 * wrist.getWristAngle() - 0.7029;
-        
-      //   // output += 8.7947 * Math.pow(10,-7) * wrist.getWristAngle() * wrist.getWristAngle() - 0.67095;
-        
-      //   wrist.moveWrist(output);
-      // }
-      // else{
         wrist.moveWrist(wristInput * 0.2);
-      // }
     }
   
     @Override
