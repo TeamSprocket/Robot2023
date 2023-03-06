ea777e3b2b1d489e8e138ddfb98994bc92273aed
MichaelW <98852023+MichaelWuhu@users.noreply.github.com>
2023-03-06 10:07:29 -0800

    added imports and test code also updated to 4.2
    idk but somethin wrong with my Main.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -10,20 +11,60 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.SwerveDriveCmd;
 
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the manifest
+ * file in the resource
  * directory.
  */
+
 public class Robot extends TimedRobot {
     Timer timer;
+
+    // This will load the file "Example Path.path" and generate it with a max
+    // velocity of 4 m/s and a max acceleration of 3 m/s^2
+    PathPlannerTrajectory examplePath = PathPlanner.loadPath("Example Path", new PathConstraints(4, 3));
+
+    // This trajectory can then be passed to a path follower such as a
+    // PPSwerveControllerCommand
+    // Or the path can be sampled at a given point in time for custom path following
+
+    // Sample the state of the path at 1.2 seconds
+    PathPlannerState exampleState = (PathPlannerState) examplePath.sample(1.2);
+
+    // Print the velocity at the sampled time
+    // System.out.println(exampleState.velocityMetersPerSecond); // <-- sys print
+    // not working :/
+
+    // This will load the file "Example Path Group.path" and generate it with a max
+    // velocity of 4 m/s and a max acceleration of 3 m/s^2
+    // for every path in the group
+    List<PathPlannerTrajectory> pathGroup1 = PathPlanner.loadPathGroup("Example Path Group",
+            new PathConstraints(4, 3));
+
+    // This will load the file "Example Path Group.path" and generate it with
+    // different path constraints for each segment
+    List<PathPlannerTrajectory> pathGroup2 = PathPlanner.loadPathGroup(
+            "Example Path Group",
+            new PathConstraints(4, 3),
+            new PathConstraints(2, 2),
+            new PathConstraints(3, 3));
 
     private final RobotContainer robotContainer = new RobotContainer();
 
     public Robot() {
-        
+
     }
 
     /**
@@ -35,10 +76,13 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * This function is called every robot packet, no matter the mode. Use this for items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
      *
-     * <p>This runs after the mode specific periodic functions, but before
+     * <p>
+     * This runs after the mode specific periodic functions, but before
      * LiveWindow and SmartDashboard integrated updating.
      */
     @Override
@@ -63,12 +107,12 @@ public class Robot extends TimedRobot {
     public void autonomousInit() {
         Command auton = robotContainer.getAutonomousCommand();
 
-        if(auton!=null) {
+        if (auton != null) {
             auton.schedule();
         }
         // timer = new Timer();
         // timer.start();
-        
+
     }
 
     /**
@@ -78,17 +122,19 @@ public class Robot extends TimedRobot {
     public void autonomousPeriodic() {
         // double time = timer.get();
         // if (time < 1) {
-        //     new SwerveDriveCmd(swerveDrive, null, null, null))
+        // new SwerveDriveCmd(swerveDrive, null, null, null))
         // }
-        
+
     }
 
-    /** This function is called once each time the robot enters teleoperated mode. */ 
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
     @Override
     public void teleopInit() {
         CommandScheduler.getInstance().cancelAll();
     }
-    
+
     /** This function is called periodically during teleoperated mode. */
     @Override
     public void teleopPeriodic() {
