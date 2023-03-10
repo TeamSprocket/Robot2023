164dcd74ca21abce30621f2eb8316f489a9dfbec
Eric Wang <108838192+sdkjg@users.noreply.github.com>
2023-03-09 18:00:28 -0800

    setting output values with control mode

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.Constants;
@@ -22,14 +23,18 @@ public class Claw extends SubsystemBase{
         claw.setInverted(false);
 
         
-        claw.setNeutralMode(NeutralMode.Coast);
+        claw.setNeutralMode(NeutralMode.Brake);
 
     }
 
     public void moveClaw(double output){
-        claw.set(output * 0.2);
+        //claw.set(output * 0.2);
 
-        // rightClaw.set(ControlMode.PercentOutput, output);
+        claw.set(ControlMode.PercentOutput, output);
+        //percent output ouputs as a percentange from -1 to 1 with 0 stopping the motor
+        //other modes are current mode, where the output is in amps,
+        //position mode, where the output is in encoder ticks or analog values
+        //and follower mode, where the output is the interger device ID (idk what this is)
     }
 
     public void actuateClaw(boolean out) {
@@ -48,8 +53,12 @@ public class Claw extends SubsystemBase{
         return false;
     }
 
+    public double getVelocity() {
+        return claw.getSelectedSensorPosition();
+    }
+
     @Override
     public void periodic() {
-        // SmartDashboard.putBoolean("[Intake] isActuated", isActuated());
+        SmartDashboard.putNumber("[Claw] RPM", getVelocity());
     }
 }
