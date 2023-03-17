aa96c403820fe6ebe1b77bbdd4ae2b3273235874
Zevzzz <46076391+Zevzzz@users.noreply.github.com>
2023-03-16 21:45:49 -0700

    Made formatting clearer for limelight align

--- src/main/java/frc/robot/commands/macro/timed/LimelightAlignTimed.java
+++ src/main/java/frc/robot/commands/macro/timed/LimelightAlignTimed.java
@@ -47,7 +47,7 @@ public class LimelightAlignTimed extends CommandBase {
     this.timer = new Timer();
     this.swerveDrive = swerveDrive;
     this.duration = duration;
-    this.verticalSpeed = -verticalSpeed;
+    this.verticalSpeed = -1.0 * verticalSpeed;
     // this.duration = duration;
 
     timer = new Timer();
