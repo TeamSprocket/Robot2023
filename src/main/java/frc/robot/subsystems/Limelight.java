5f6bcffdf1f412fbf8409f1de17e82235c121ad7
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2024-01-10 16:53:34 -0800

    Update Limelight subsystem

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,6 +8,18 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.SwerveDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight extends SubsystemBase{
 
@@ -19,9 +31,53 @@ public class Limelight extends SubsystemBase{
     private double x = tx.getDouble(0.0);
     private double y = ty.getDouble(0.0);
 
-    public Limelight() {
-        
+    SwerveDrive swerveDrive;
+    PIDController pidController;
+
+    public Limelight(SwerveDrive swerveDrive) {
+        this.swerveDrive = swerveDrive;
+        this.pidController = new PIDController(Constants.Drivetrain.kLimelightAlignP, Constants.Drivetrain.kLimelightAlignI, Constants.Drivetrain.kLimelightAlignD);
+        this.pidController.setSetpoint(0);
     }
+
+    public void setSpeeds() {
+
+    double tXOffset = table.getEntry("tx").getDouble(0.0);
+    double output = -1 * pidController.calculate(tXOffset);
+
+    ChassisSpeeds chassisSpeeds;
+    
+    if (Constants.Drivetrain.kIsFieldOriented) {
+
+        double headingRad = Math.toRadians(swerveDrive.getHeading());
+        chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(0, output, 0, new Rotation2d(headingRad));
+    } 
+    else { 
+
+        chassisSpeeds = new ChassisSpeeds(0, output, 0);
+    }
+
+    SwerveModuleState[] moduleStates = Constants.Drivetrain.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+    swerveDrive.setModuleStates(moduleStates);
+ }
+
+public void setAprilTagPipeline() {
+    table.getEntry("pipeline").setNumber(0);
+ }
+
+ public double getXoffset() {
+    return table.getEntry("tx").getDouble(0.0);
+ }
+
+ public void changeAlliance() {
+    DriverStation.Alliance color = DriverStation.getAlliance();
+    if (color == DriverStation.Alliance.Blue) {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose_wpiblue");
+    }
+    else if (color == DriverStation.Alliance.Red) {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose_wpired");
+    }
+ }
 
     public Pose2d getPos() {
         Translation2d tPos = new Translation2d(x, y);
