3781bc5d7e16a16cc57e71436001c8729f0a73da
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2024-01-10 16:55:29 -0800

    Update LimelightAlign command

--- src/main/java/frc/robot/commands/macro/LimelightAlign.java
+++ src/main/java/frc/robot/commands/macro/LimelightAlign.java
@@ -17,66 +17,29 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import frc.robot.subsystems.LimelightSubsystem;
+
 
 public class LimelightAlign extends CommandBase {
-  NetworkTable table;
-  double tValid = 1;
-  double tXOffset;
-  Timer timer;
-  SwerveDrive swerveDrive;
-  PIDController pidController;
-  // NetworkTableEntry tValid, tXOffset, tYOffset, tArea;
-  /** Creates a new LimelightAlign. */
-  public LimelightAlign(SwerveDrive swerveDrive) {
-    this.timer = new Timer();
-    this.swerveDrive = swerveDrive;
-    // this.duration = duration;
 
-    this.pidController = new PIDController(Constants.Drivetrain.kLimelightAlignP, Constants.Drivetrain.kLimelightAlignI, Constants.Drivetrain.kLimelightAlignD);
-    this.pidController.setSetpoint(0);
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final LimelightSubsystem;
+
+  public LimelightAlign(LimelightSubsystem limelightSubsystem) {
+    this.LimelightSubsystem = limelightSubsystem;
+
+    addRequirements(limelightSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // timer.reset();
     Constants.isEnabled = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // timer.start();
-    System.out.println("ALIGNING");
-
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    tValid = table.getEntry("tv").getDouble(0.0); // 0 = invalid, 1 = valid
-    tXOffset = table.getEntry("tx").getDouble(0.0); // x offset -27 to 27 deg
-    // double tYOffset = table.getEntry("ty").getDouble(0.0); // y offset -20.5 to 20.5 deg
-    // double tArea = table.getEntry("ty").getDouble(0.0); // area of target in img (percent of total img)
-    double output = -1 * pidController.calculate(tXOffset);
-    setSpeeds(output);
-
-  }
-
-
-
-  public void setSpeeds(double output) {
-    ChassisSpeeds chassisSpeeds;
-    if (Constants.Drivetrain.kIsFieldOriented) {
-      double headingRad = Math.toRadians(swerveDrive.getHeading());
-      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-          0, output, 0, new Rotation2d(headingRad));
-    } else { 
-      chassisSpeeds = new ChassisSpeeds(0, output, 0);
-    }
-
-    // Calculate module states per module
-    SwerveModuleState[] moduleStates = Constants.Drivetrain.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-  
-    // Apply to modules
-    swerveDrive.setModuleStates(moduleStates);
+    limelightSubsystem.setSpeeds();
   }
 
   // Called once the command ends or is interrupted.
@@ -89,16 +52,6 @@ public class LimelightAlign extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return tValid == 0.0;
-    // return true;
+    return true;
   }
-
-  
-
-
-
-
-  
-
-
 }
